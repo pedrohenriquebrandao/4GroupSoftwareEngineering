1c3bf7f14a5db90f36893aa75b904944c1038e8c
taisonmachado <taison.machado@hotmail.com>
2018-06-12 23:02:58 -0300

    Alterações tela de cadastro de produtor

--- resources/views/cadastrarProdutor.blade.php
+++ resources/views/cadastrarProdutor.blade.php
@@ -45,43 +45,65 @@
 
 					<div class="wrap-input100 validate-input" data-validate="CNPJ is required">
 						<span class="label-input100">CNPJ</span>
-						<input class="input100" type="senha" name="pass" placeholder="Digite o CNPJ">
+						<input class="input100" type="text" name="cnpj" placeholder="Digite o CNPJ">
 						<span class="focus-input100" data-symbol="&#xf10a;"></span>
 					</div>
-
+                    
+                    <br>
                     <center><h2>Endereço</h2></center>
 
                     <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
 						<span class="label-input100">Rua</span>
-						<input class="input100" type="senha" name="pass" placeholder="Digite o nome da rua">
+						<input class="input100" type="text" name="rua" placeholder="Digite o nome da rua">
 						<span class="focus-input100" data-symbol="&#xf10a;"></span>
 					</div>
 
                     <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
 						<span class="label-input100">Bairro</span>
-						<input class="input100" type="senha" name="pass" placeholder="Digite o nome do Bairro">
+						<input class="input100" type="text" name="bairro" placeholder="Digite o nome do Bairro">
+						<span class="focus-input100" data-symbol="&#xf10a;"></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
+						<span class="label-input100">CEP</span>
+						<input class="input100" type="text" name="cep" placeholder="Digite o CEP">
+						<span class="focus-input100" data-symbol="&#xf10a;"></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
+						<span class="label-input100">Número</span>
+						<input class="input100" type="text" name="num" placeholder="Digite o número">
 						<span class="focus-input100" data-symbol="&#xf10a;"></span>
 					</div>
 
                     <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
 						<span class="label-input100">Cidade</span>
-						<input class="input100" type="senha" name="pass" placeholder="Digite o nome da Cidade">
+						<input class="input100" type="text" name="cidade" placeholder="Digite o nome da Cidade">
+						<span class="focus-input100" data-symbol="&#xf10a;"></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
+						<span class="label-input100">Estado</span>
+						<input class="input100" type="text" name="estado" placeholder="Digite o Estado">
+						<span class="focus-input100" data-symbol="&#xf10a;"></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate="CNPJ is required">
+						<span class="label-input100">Complemento</span>
+						<input class="input100" type="text" name="compl" placeholder="Digite o Complemento">
 						<span class="focus-input100" data-symbol="&#xf10a;"></span>
 					</div>
 
                     <div class="row">
-                        <div class="wrap-input100 validate-input col-md-5" data-validate="CNPJ is required">
-						    <span class="label-input100">Cidade</span>
-						    <input class="input100" type="senha" name="pass" placeholder="Digite o nome da Cidade">
-						    <span class="focus-input100" data-symbol="&#xf10a;"></span>
-					    </div>
-                        <div class="wrap-input100 validate-input col-md-7" data-validate="CNPJ is required">
-						    <span class="label-input100">Cidade</span>
-						    <input class="input100" type="senha" name="pass" placeholder="Digite o nome da Cidade">
-						    <span class="focus-input100" data-symbol="&#xf10a;"></span>
-					    </div>
+                        <div class="form-check col-sm-6">
+                            <input type="radio" class="form-check-input" name="zona" id="radioUrbana" value="option1" checked>
+                            <label class="form-check-label" for="radioUrnaba">Zona Urbana</label>
+                        </div>
+                        <div class="form-check col-sm-6">
+                            <input type="radio" class="form-check-input" name="zona" id="radioRural" value="option2" checked>
+                            <label class="form-check-label" for="radioRural">Zona Rural</label>  
+                        </div>
                     </div>
-
 
 
 					<div class="container-login100-form-btn">
