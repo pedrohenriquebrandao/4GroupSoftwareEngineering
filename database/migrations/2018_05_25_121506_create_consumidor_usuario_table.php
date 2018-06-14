52042a8e6291f133b015b3d740510b67bdfb6190
carmonapat <carmonapat@github.com>
2018-06-14 02:24:27 -0300

    migrations do banco

--- database/migrations/2018_05_25_121506_create_consumidor_usuario_table.php
+++ database/migrations/2018_05_25_121506_create_consumidor_usuario_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateConsumidorUsuarioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('consumidor_usuario', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('telefone', 10);
+            $table->string('nascimento', 8);
+            $table->string('sexo', 10);
             $table->timestamps();
         });
     }
@@ -29,6 +29,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('consumidor_usuario');
     }
+
 }
--- database/migrations/2018_05_25_124707_create_consumidor_assinatura_table.php
+++ database/migrations/2018_05_25_124707_create_consumidor_assinatura_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateConsumidorAssinaturaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('consumidor_assinatura', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('dataDeAssinatura', 8);
+            $table->float('preco');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -29,6 +28,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('consumidor_assinatura');
     }
+
+
 }
