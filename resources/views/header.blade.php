139ff48cc6c18c46f161abe079a5c141e9ab50ba
Leonardo Santos <leomachadoms@gmail.com>
2018-06-15 21:20:20 -0300

    detalhes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,16 +1,17 @@
 <style>
-    .bg-red{
+    .bg-red {
         background-color: rgb(173, 30, 25);
     }
+
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-red navbar-fixed-top" role="navigation">
     <div class="container">
         <div>
             <a style="margin-left: 0px" class="navbar-brand js-scroll-trigger" href="#page-top">
-                
-            <img src="{!! asset('image/logo.png') !!}" height= "130">
-            <img src="{!! asset('image/nomelogo.png') !!}" height= "120">            
+
+                <img src="{!! asset('image/logo.png') !!}" height="80">
+                <img src="{!! asset('image/nomelogo.png') !!}" height="70">
             </a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,21 +20,29 @@
         </div>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+                <!-- ->
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+
+                <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="#pesquisar">
                         <i class="fa fa-search fa-2x" aria-hidden="true"></i>
                     </a>
                 </li>
+                <!-- -->
                 <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="#carrinho">
+                        CARRINHO
                         <i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i>
                     </a>
-                </li>     
+                </li>
                 <li class="nav-item">
                     <a class="nav-link js-scroll-trigger" href="/login">
+                        SUA CONTA
                         <i class="fa fa-user fa-2x" aria-hidden="true"></i>
                     </a>
-                </li>   
+                </li>
             </ul>
         </div>
     </div>
