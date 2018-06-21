476247b354901a964f4b2942cc21b2137b2ec889
karolynelima <kmrlima36@gmail.com>
2018-06-21 09:59:01 -0300

    Recolocação do file web

---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('cadastrar', function () {
+    return view('cadastrar');
+});
+Route::get('admin', function () {
+    return view('admin');
+});
+Route::get('recsenha', function () {
+    return view('recsenha');
+});
+Route::get('cadastrarProdutor', function () {
+    return view('cadastrarProdutor');
+});
+Route::get('recsenhaconfirm', function () {
+    return view('recsenhaconfirm');
+});
+Route::get('perfilProdutor', function () {
+    return view('perfilProdutor');
+});
+Route::get('carrinho', function () {
+    return view('carrinho');
+});
+<<<<<<< HEAD
+Route::get('indexProdutor', function () {
+    return view('indexProdutor');
+});
+
+Route::get('tables', function () {
+    return view('tables');
+});
+Route::get('forms', function () {
+    return view('forms');
+});
+Route::get('chartsProdutor', function () {
+    return view('chartsProdutor');
+=======
+Route::get('pagamento', function () {
+    return view('pagamento');
+>>>>>>> ed99ef1c212785ed945344ae40f11c185463d252
+});
+
+Route::post("/cadUsuario", "LoginController@cadLogin");
+
+Route::post("/cadProdutor", "ControllerProdutor@cadProdutor"); //Cria cadastro de Produtor no Banco de Dados.
