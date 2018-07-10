c6b30fedd179eb06df13b1109c0c783c75675101
Ennio Sousa <ennio@enniosousa.com.br>
2018-07-10 15:45:54 -0300

    Preencher dinamicamente

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -1,12 +1,13 @@
 <div class="col-lg-3 col-md-6 mb-4">
     <div class="card">
-        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <img class="card-img-top" src="<?= $produto->imagem ?? 'http://vargasmotos.com.br/images/sem_foto.png' ?>" alt="">
         <div class="card-body">
-            <h4 class="card-title">Produto</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+        <h4 class="card-title"><?= $produto->nome ?? 'Sem nome' ?></h4>
+            <p class="card-text"><?= $produto->descricao ?? 'Não tem descricao' ?></p>
         </div>
         <div class="card-footer">
-            <a href="#" class="btn btn-primary">+ Carrinho</a>
+            <a href="/carrinho" class="btn btn-primary">+ Carrinho</a>
+        <a href="/visao-produto/{{ $produto->id }}" class="btn btn-secundary">Ver produto</a>
         </div>
     </div>
 </div>
