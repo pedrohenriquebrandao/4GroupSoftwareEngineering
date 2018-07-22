f4a006a2acbc5b64178f47409d6439aff28478b4
gabriel-cruz <gabrielcruz7400@gmail.com>
2018-07-21 22:52:01 -0300

    Todas gerações de seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,19 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CarrinhoTableSeeder::class);
         $this->call(EnderecoTableSeeder::class);
-        //$this->call(LoginTableSeeder::class);
         $this->call(UsuariosTableSeeder::class);
         $this->call(ProdutorTableSeeder::class);
         $this->call(CartaoTableSeeder::class);
-        $this->call(ProdutorTableSeeder::class);
+        $this->call(ProdutoTableSeeder::class);
+        $this->call(PunicaoTableSeeder::class);
+        $this->call(ComentarioTableSeeder::class);
+        $this->call(LojaTableSeeder::class);
+        $this->call(RendaTableSeeder::class);
+        $this->call(ValorTableSeeder::class);
+        $this->call(AssinaturaTableSeeder::class);
+        //$this->call(LoginTableSeeder::class);
+        //$this->call(AdminTableSeeder::class);
+
+
     }
 }
